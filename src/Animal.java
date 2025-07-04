372618949a5610b80a3f891d4d6b830aff2d6e34
DEVA <DEVA@LAPTOP-0NN4R7OE>
2025-07-04 18:58:10 +0530

    commit change

--- src/Animal.java
+++ src/Animal.java
@@ -2,8 +2,10 @@
 public class Animal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("HelloWorld");
+		System.out.println("Devidas");
+		
 
 	}
 
